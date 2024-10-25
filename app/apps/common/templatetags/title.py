e9d1956048f4c24b21bde7acf1b791f89a591fbc
Herculino Trotta <netotrotta@gmail.com>
2024-10-25 15:47:22 -0300

    fix: double quotes inside double quotes

--- app/apps/common/templatetags/title.py
+++ app/apps/common/templatetags/title.py
@@ -8,6 +8,6 @@ register = template.Library()
 def site_title(value):
     value = value.strip()
     if value:
-        return f"{value} {settings.TITLE_SEPARATOR or '::'} {settings.SITE_TITLE or "SITE_TITLE NOT SET"}"
+        return f"{value} {settings.TITLE_SEPARATOR or '::'} {settings.SITE_TITLE or 'SITE_TITLE NOT SET'}"
     else:
         return settings.SITE_TITLE or "SITE_TITLE NOT SET"
