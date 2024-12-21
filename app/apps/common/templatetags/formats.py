68992839c21734279f08ddf960dbd65ad094eb80
Herculino Trotta <netotrotta@gmail.com>
2024-12-21 16:30:17 -0300

    refactor: remove debug prints

--- app/apps/common/templatetags/formats.py
+++ app/apps/common/templatetags/formats.py
@@ -7,11 +7,9 @@ register = template.Library()
 
 @register.simple_tag
 def get_thousand_separator():
-    print(get_format("THOUSAND_SEPARATOR"))
     return get_format("THOUSAND_SEPARATOR")
 
 
 @register.simple_tag
 def get_decimal_separator():
-    print(get_format("DECIMAL_SEPARATOR"))
     return get_format("DECIMAL_SEPARATOR")
