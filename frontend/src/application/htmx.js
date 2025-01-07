f81f1d83fd1b3cd93dc1e6c89690438090b7ae79
Herculino Trotta <netotrotta@gmail.com>
2025-01-07 16:16:26 -0300

    fix(calculator): rounding errors

--- frontend/src/application/htmx.js
+++ frontend/src/application/htmx.js
@@ -2,16 +2,21 @@ import _hyperscript from 'hyperscript.org/dist/_hyperscript.min';
 import './_htmx.js';
 import Alpine from "alpinejs";
 import mask from '@alpinejs/mask';
-import { create, all } from 'mathjs';
+import {create, all} from 'mathjs';
 
 window.Alpine = Alpine;
 window._hyperscript = _hyperscript;
-window.math = create(all, { });
+window.math = create(all, {
+    number: 'BigNumber',      // Default type of number:
+                              // 'number' (default), 'BigNumber', or 'Fraction'
+    precision: 64,            // Number of significant digits for BigNumbers
+    relTol: 1e-60,
+    absTol: 1e-63
+});
 
 Alpine.plugin(mask);
 Alpine.start();
 _hyperscript.browserInit();
-
 
 
 const successAudio = new Audio("/static/sounds/success.mp3");
