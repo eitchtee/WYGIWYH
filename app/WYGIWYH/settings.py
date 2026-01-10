ab4075c5009ae89be1c7a27ae80000ef9b567a4d
Herculino Trotta <netotrotta@gmail.com>
2026-01-10 17:44:57 -0300

    fix: missing list close

--- app/WYGIWYH/settings.py
+++ app/WYGIWYH/settings.py
@@ -437,6 +437,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
