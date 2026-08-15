33f0904a0f75826caf29215c22f82ff6995285e0
Herculino Trotta <hello@herculino.com>
2026-08-15 15:10:10 -0300

    feat(dca): add chart zoom navigation

--- frontend/src/js/charts.js
+++ frontend/src/js/charts.js
@@ -1,5 +1,6 @@
 import Chart from 'chart.js/auto';
 import {SankeyController, Flow} from 'chartjs-chart-sankey';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
-Chart.register(SankeyController, Flow);
+Chart.register(SankeyController, Flow, zoomPlugin);
 window.Chart = Chart;
